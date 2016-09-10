73fa1a54ff2d07cdf74d82abc30429bfad531bb5
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2016-09-10 19:05:31 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'csvparser',
+  packages = ['csvparser'],
+  version = '0.1',
+  description = 'Lib for parsing csv files',
+  author = 'Bartłomiej Bęczkowski',
+  author_email = 'beczkowb@gmail.com',
+  url = 'https://github.com/beczkowb/csvparser',
+  download_url = 'https://github.com/beczkowb/csvparser.git',
+  keywords = ['csv', 'parsing'],
+  classifiers = [],
+)
