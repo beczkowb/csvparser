8bf24bb7b14317598dda3129f94ebae56527bfea
Bartłomiej Bęczkowski <bartlomiej.beczkowski@bpo.pl>
2016-09-15 14:06:35 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
   name = 'csvparser',
   packages = ['csvparser'],
-  version = '0.1.2',
-  description = 'Library for parsing csv files using declared django-style parser classes.',
+  version = '0.2.0',
+  description = 'Library for parsing and validating csv files using declared django-style parser classes.',
   author = 'Bartłomiej Bęczkowski',
   author_email = 'beczkowb@gmail.com',
   url = 'https://github.com/beczkowb/csvparser',
   download_url = 'https://github.com/beczkowb/csvparser.git',
-  keywords = ['csv', 'parsing'],
+  keywords = ['csv', 'parsing', 'django'],
   classifiers = [],
+  license = 'MIT',
 )
