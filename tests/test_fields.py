8ed3667f9340ce407ee5dda2e594a8423bea832f
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2016-09-28 22:52:04 +0200

    test case for DateField

---
+++ tests/test_fields.py
@@ -0,0 +1,25 @@
+import unittest
+from csvparser import fields
+
+
+class TestClassWithDateField(object):
+    date_attr1 = fields.DateField(date_format='%Y-%m-%d')
+    date_attr2 = fields.DateField(date_format='%Y-%m-%d')
+
+
+class DateFieldTestCase(unittest.TestCase):
+    def test_date_format1(self):
+        test_object = TestClassWithDateField()
+        test_object.date_attr1 = '2016-07-09'
+        test_object.date_attr2 = '2017-12-27'
+
+        self.assertEqual(test_object.date_attr1.year, 2016)
+        self.assertEqual(test_object.date_attr1.month, 7)
+        self.assertEqual(test_object.date_attr1.day, 9)
+
+        self.assertEqual(test_object.date_attr2.year, 2017)
+        self.assertEqual(test_object.date_attr2.month, 12)
+        self.assertEqual(test_object.date_attr2.day, 27)
+
+    def test_date_format2(self):
+        pass
